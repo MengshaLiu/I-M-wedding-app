0705505a0fe416afb8ca586a0a124d092a4888c8
MengshaLiu <mengshaliu2@gmail.com>
2026-08-18 14:03:02 +1000

    Adjust logos size

--- wedding-site/frontend/app/(guest)/layout.tsx
+++ wedding-site/frontend/app/(guest)/layout.tsx
@@ -60,7 +60,7 @@ export default async function GuestLayout({ children }: { children: React.ReactN
           }}>
             <Link href="/" style={{ textDecoration: "none", lineHeight: 0 }}>
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/wedding/logo.png" alt="I & M" style={{ height: 105, width: "auto", mixBlendMode: "screen" }} />
+              <img src="/wedding/logo.png" alt="I & M" style={{ height: 150, width: 150, mixBlendMode: "screen"}} />
             </Link>
             <div style={{ display: "flex", gap: 24, flexWrap: "nowrap" }}>
               {links.map(({ label, href }) => (
@@ -107,7 +107,7 @@ export default async function GuestLayout({ children }: { children: React.ReactN
         }}>
           <Link href="/" style={{ textDecoration: "none", lineHeight: 0 }}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/wedding/chinese-logo.png" alt="I & M" style={{ height: 105, width: "auto", mixBlendMode: "screen" }} />
+            <img src="/wedding/chinese-logo.png" alt="I & M" style={{ height: 180, width: 180, mixBlendMode: "screen"}} />
           </Link>
           <div style={{ display: "flex", gap: 24, flexWrap: "nowrap" }}>
             {receptionLinks.map(({ label, chinese, href }) => (
